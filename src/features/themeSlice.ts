fbb3b640a579c0067343c46b60af69d7baf49af6
Patrick <patrick.juers@t-online.de>
2024-04-16 10:35:52 +0200

    fixed themes

--- src/features/themeSlice.ts
+++ src/features/themeSlice.ts
@@ -3,11 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const themeSlice = createSlice({
     name: 'theme',
     initialState: {
-        isDark: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        isDark: localStorage.getItem('isDark') === 'true' ? true : 
+        localStorage.getItem('isDark') === 'false' ? false : 
+        window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     },
     reducers:{
         handleTheme: (state)=>{
             state.isDark = !state.isDark
+            localStorage.setItem('isDark', String(state.isDark));
+
         }
     }
 
